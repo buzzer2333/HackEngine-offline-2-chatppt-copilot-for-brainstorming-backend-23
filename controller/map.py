583ab867f9a5eb0820030d18e4b4eacf84a12ff9
XN1ng <48912288+XN1ng@users.noreply.github.com>
2023-04-09 14:24:59 +0800

    detail controller开发

--- controller/map.py
+++ controller/map.py
@@ -1,15 +1,33 @@
 import json
+import uuid
 from log import Log
 from flask import request, jsonify, Blueprint, session, make_response
 from service.mapping import MindMap, default
+from service.detail import EntityInfos
 
 map_blue = Blueprint("map", __name__)
 
 # uuid -> mindMap
 user2map = dict()
+# uuid -> nodeInfos
+user2info = dict()
+
+
+def get_user_id() -> str:
+    # 存储用户mindMap
+    user_id = session.get('user_id')
+    if user_id is None:
+        user_id = uuid.uuid4()
+        session['user_id'] = user_id
+        Log.errorf("get user id failed, re-generate one as %s", user_id)
+        return user_id
+    Log.infof("get user id as %s", user_id)
+    return user_id
+
 
 @map_blue.route("/expand", methods=["POST"])
 def expand():
+    # todo :: biz
     params = request.get_data()
     params = json.loads(params)
     Log.infof("get input as %s", json.dumps(params))
@@ -18,27 +36,31 @@ def expand():
 
 @map_blue.route("/init", methods=["POST"])
 def init():
-    input = request.get_data()
-    Log.infof("get input as %s", input)
+    inputs = request.get_data()
+    Log.infof("get inputs as %s", inputs)
     m = MindMap()
-    m.ask_for_initial_graph(input)
+    m.ask_for_initial_graph(inputs)
 
     # 存储用户mindMap
-    uuid = session['user_id']
-    if uuid is None:
-        Log.errorf("get user id failed")
-        return jsonify({"err":"get user info failed"})
-    Log.infof("get user id as %s", uuid)
-    user2map[uuid] = m
+    userid = get_user_id()
+    user2map[userid] = m
     Log.infof("get user2Map as %s", user2map)
     rsp = make_response(json.dumps(m.root, default=default))
     rsp.headers["Content-Type"] = "application/json; charset=utf-8"
     return rsp
 
-@map_blue.route("/detail", methods=["POST"])
+
+@map_blue.route("/detail", methods=["GET"])
 def detail():
-    string = request.get_data()
-    Log.infof("get input as %s", string)
-
-
-
+    node_id = request.args.get("id")
+    node_label = request.args.get("label")
+    query = request.args.get("query")
+    Log.infof("get id as %s, label as %s", node_id, node_label)
+    user_id = get_user_id()
+    if user_id not in user2info:
+        entity_info = EntityInfos()
+        output = entity_info.ask_for_more_detail(query, node_id)
+        user2info[user_id] = entity_info
+    else:
+        output = user2info[user_id].ask_for_more_detail(query, node_id)
+    return jsonify({"data": output})
--- service/detail.py
+++ service/detail.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass, asdict
 from module.message import Message
 from module.conservation import DETAIL_CONVERSATION
 
+
 def ask_chatgpt_detail(conversation: List[Message]) -> Tuple[str, List[Message]]:
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -16,29 +17,16 @@ def ask_chatgpt_detail(conversation: List[Message]) -> Tuple[str, List[Message]]
 
     return msg.content, conversation + [msg]
 
+
 class EntityInfos:
     """A class that represents entity infos
     """
 
-    def __init__(self, **nodes) -> None:
-        self.nodesInfo = [] if nodes is None else nodes
-        self.save()
+    def __init__(self, nodes: dict = None) -> None:
+        self.conversation = ""
+        self.nodesInfo = dict() if nodes is None else nodes
 
-    @classmethod
-    def load(cls):
-        """Load EntityInfo from session state if it exists
-        
-        Returns: entityInfos
-        """
-        if "entityInfos" in st.session_state:
-            return st.session_state["entityInfos"]
-        return cls()
-
-    def save(self) -> None:
-        # save to session state
-        st.session_state["entityInfos"] = self
-
-    def ask_for_more_detail(self, query: str, nodeID: str) -> None:
+    def ask_for_more_detail(self, query: str, nodeID: str) -> str:
         """Ask GPT-3 to construct a graph from scrach.
 
         Args:
